daab67a813138ef55bd25c419cc722a5babb509c
atooxa <attooxa@yandex.ru>
2017-11-23 00:44:32 +0500

    Add UDP ping test.

---
+++ client.js
@@ -0,0 +1,26 @@
+const dgram = require('dgram');
+const socket = dgram.createSocket('udp4');
+const {
+  PORT,
+  HOST
+} = require('./config');
+let sentTimestamp = null;
+
+socket.on('message', (message, remote) => {
+  console.log(`Ping: ${Date.now() - sentTimestamp}ms`);
+
+  setTimeout(sendMessageToServer.bind(this, { message: `Hello server!` }), 500);
+});
+
+function sendMessageToServer(message) {
+  socket.send(JSON.stringify(message), PORT, HOST, (err) => {
+    if (err) return console.error(err);
+
+    sentTimestamp = Date.now();
+  });
+}
+
+sendMessageToServer({ message: `Hello server!` });
+
+
+
---
+++ server.js
@@ -0,0 +1,18 @@
+const dgram = require('dgram');
+const socket = dgram.createSocket('udp4');
+const {
+  PORT,
+  HOST
+} = require('./config');
+
+socket.on('listening', () => {
+  console.log('UDP Server listening on ' + socket.address().address + ":" + socket.address().port);
+});
+
+socket.on('message', (message, remote) => {
+  console.log(`Message from: ${remote.address}:${remote.port} - ${message}`);
+
+  socket.send(JSON.stringify({ message: 'Hello client' }), remote.port, remote.address);
+});
+
+socket.bind(PORT, HOST);
